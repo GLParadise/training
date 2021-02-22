bddda0c6f965b58f6362f84b3d69c80116793037
Gabriel <gloganparadise@gmail.com>
2021-02-21 22:40:25 -0500

    Started working on webscraping

---
+++ WebScraper/main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    f = open("scrape.csv", "w")
+
+    url="https://www.google.com/search?q=penguin&rlz=1C1CHBF_enUS779US779&oq=penguin&aqs=chrome.0.69i59l2j0i67j0i67i433j46i131i433j46i67i131i433j46i131i433j0i67.1728j0j7&sourceid=chrome&ie=UTF-8"
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    f.write(soup.prettify())
+    print('done')
